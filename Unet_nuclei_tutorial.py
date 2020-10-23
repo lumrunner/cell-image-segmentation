a7f8df678fbc2ce9025c319dd0b4af0b1021e9a4
lumrunner <lllllll159@yahoo.com>
2020-10-23 01:48:38 -0400

    model updated

--- Unet_nuclei_tutorial.py
+++ Unet_nuclei_tutorial.py
@@ -2,16 +2,18 @@
 # Author: Liam McGunnigle
 # Date: 10/21/2020
 # License: MIT
+# Version: 1.1
 #######################################
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import random
 
 from tqdm import tqdm
 from skimage.io import imread, imshow
 from skimage.transform import resize
+from datetime import datetime
 
 seed = 42
 np.random.seed = seed
@@ -25,56 +27,31 @@ test_ids = next(os.walk(TEST_PATH))[1]
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 IMG_CHANNELS = 3
-
-
-#importing images
-X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.float)
-Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
-
-print('resizing training images and masks')
-for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
-    path = TRAIN_PATH + id_
-    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
-    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
-    X_train[n] = img
-    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
-    for mask_file in next(os.walk(path + '/masks/'))[2]:
-        mask_ = imread(path + '/masks/' + mask_file)
-        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True), axis=-1)
-        mask = np.maximum(mask, mask_)
-    Y_train[n] = mask
-
-print('Resizing test images')
-X_test = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
-sizes_test = []
-for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
-    path = TEST_PATH + id_
-    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
-    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
-    X_test[n] = img
+num_imgs = len(train_ids)
 
 # build model
+print('building model')
 inputs = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
 
 s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
 c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
 c1 = tf.keras.layers.Dropout(0.1)(c1)
-c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+p1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
 p1 = tf.keras.layers.MaxPooling2D((2,2))(c1)
 
 c2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
 c2 = tf.keras.layers.Dropout(0.1)(c2)
-c2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+p2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
 p2 = tf.keras.layers.MaxPooling2D((2,2))(c2)
 
 c3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
 c3 = tf.keras.layers.Dropout(0.2)(c3)
-c3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+p3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
 p3 = tf.keras.layers.MaxPooling2D((2,2))(c3)
 
 c4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
 c4 = tf.keras.layers.Dropout(0.2)(c4)
-c4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+p4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
 p4 = tf.keras.layers.MaxPooling2D((2,2))(c4)
 
 c5 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
@@ -112,6 +89,36 @@ model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 model.summary()
 
+model.save('empty_nuclei_model.h5')
+
+#importing images
+print('Importing images')
+X_train = np.zeros(((num_imgs), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros(((num_imgs), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+time_started = datetime.now()
+
+for n, id_ in tqdm(enumerate(train_ids), total=num_imgs):
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_train[n] = img
+    mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    for mask_file in next(os.walk(path + '/masks/'))[2]:
+        mask_ = imread(path + '/masks/' + mask_file)
+        mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True), axis=-1)
+        mask = np.maximum(mask, mask_)
+    Y_train[n] = mask
+
+print('Resizing test images')
+X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+sizes_test = []
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    path = TEST_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+    X_test[n] = img
+
+
 #############################
 #model checkpoint
 checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_nuclei.h5', verbose=1, save_best_only=True)
@@ -125,7 +132,7 @@ callbacks = [
 results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=100, callbacks=callbacks)
 
 #############################
-idx = random.randint(0, len(X_train))
+idx = np.random.randint(0, len(X_train))
 
 preds_train = model.predict(X_train[:int(X_train.shape[0] * 0.9)], verbose=1)
 preds_val = model.predict(X_train[:int(X_train.shape[0] * 0.9)], verbose=1)
@@ -136,7 +143,7 @@ preds_val_t = (preds_val > 0.5).astype(np.uint8)
 preds_test_t = (preds_test > 0.5).astype(np.uint8)
 
 #perform sanity check
-ix = random.randint(0, len(preds_train_t))
+ix = np.random.randint(0, len(preds_train_t))
 imshow(X_train[ix])
 plt.show()
 imshow(np.squeeze(Y_train[ix]))
